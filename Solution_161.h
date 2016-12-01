1f50d521d463fa23d2363f35057dc97296255dd5
keyeqing <dearkristin1989@gmail.com>
2016-12-01 22:39:33 +0800

    add 161 v3

--- Solution_161.h
+++ Solution_161.h
@@ -66,6 +66,21 @@ public:
         return false;
 
     }
+
+    bool isOneEditDistance_v3(string s, string t){
+        for (int i = 0; i < min(s.size(),t.size()); ++i) {
+            if (s[i] != t[i]) {
+                if (s.size() == t.size()) {
+                    return s.substr(i + 1) == t.substr(i + 1);
+                } else if (s.size() > t.size()) {
+                    return s.substr(i + 1) == t.substr(i);
+                } else {
+                    return s.substr(i) == t.substr(i + 1);
+                }
+            }
+        }
+        return abs(s.size() - t.size()) == 1;
+    }
 };
 
 bool test_161();
